44352dc000ca86a06572869666f7834883d86a0c
yujiniii <youjin030700@gmail.com>
2022-09-12 23:23:14 +0900

    fix: 관계수정

--- models/index.js
+++ models/index.js
@@ -12,14 +12,25 @@ const Country = require("./country.model")(sequelize);
 const Coupon = require("./coupon.model")(sequelize);
 const Order = require("./order.model")(sequelize);
 const Delivery = require("./delivery.model")(sequelize);
+const User = require("./user.model")(sequelize)
 
 // Order : Delivery ==> 1:1
 Order.hasOne(Delivery, { foreignKey: "deliveryId" });
 Delivery.belongsTo(Order, { foreignKey: "deliveryId" });
 
 // Order : Country ==> 1:1
-Order.hasOne(Country, { foreignKey: "countryId" });
+Order.hasMany(Country, { foreignKey: "countryId" });
 Country.belongsTo(Order, { foreignKey: "countryId" });
+
+// User : Order ==> 1:N
+User.hasMany(Order, { foreignKey: "userId" });
+Order.belongsTo(User, { foreignKey: "userId" });
+
+// Order : Coupon ==> 1:N
+Order.hasMany(Coupon, { foreignKey: "couponId" });
+Coupon.belongsTo(Order, { foreignKey: "couponId" });
+
+
 
 const db = {};
 db.sequelize = sequelize;
@@ -28,5 +39,6 @@ db.Country = Country;
 db.Coupon = Coupon;
 db.Order = Order;
 db.Delivery = Delivery;
+db.User = User;
 
 module.exports = db;
